a8a39f6db68c47d8a563831beadcee690cf1a126
Bui Xuan Dong <117022784+xuandongcoder@users.noreply.github.com>
2023-10-26 16:40:48 +0700

    Add files via upload

---
+++ DuongTronHH.java
@@ -0,0 +1,59 @@
+package cit.ctu;
+import java.util.Scannner;
+
+public class DuongTronHH extends DuongTron{
+	
+	private String maubien;
+	private String maunen;
+	public DuongTronHh(){
+		super();
+		maubien = null;
+		maunen = null;
+	}
+	public DuongTronHH(Diem tam, int bankinh, String maubien, String maunen) {
+		super(tam, bankinh);
+		this.maubien = maubien;
+		this.maunen = maunen;
+	}
+	public DuongTronHH(int x, int y, int bk, String maubien, String maunen) {
+		super(x, y, bk);
+		this.maubien = maubien;
+		this.maunen = maunen;
+	}
+	public DuongTronHH(DuongTronHH dt) {
+		super(dt);
+		this.maubien = dt.maubien;
+		this.maunen = dt.maunen;
+	}
+	public makeCopy(DuongTronHH d) {
+		super.SaoChepSau(d);
+		maubien = d.maubien;
+		maunen = d.maunen;
+	}
+	public void nhap() {
+		super.nhapDuongTron();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap mau bien: ");
+		this.maubien = sc.nextLine();
+		System.out.println("Nhap mau nen: ");
+		this.maunen = sc.nextLine();
+		
+	}
+	public void hienthi() {
+		super.hienThiDT();
+		System.out.println("Mau bien: " + maubien);
+		System.out.println("Mau nen: " + maunen);
+	}
+	public String toSring() {
+		
+	}
+	public boolean equals() {
+		
+	}
+	public String layMauBien() {
+		return maubien;
+	}
+	public String layMauNen() {
+		return maunen;
+	}
+}
---
+++ SDDuongTronHH.java
@@ -0,0 +1,26 @@
+package cit.ctu;
+
+public class SDDuongTronHH {
+	public static void main(String[] args) {
+		Diem d1 = new Diem(2, 5);
+		DuongTronHH gc1 = new DuongTrongHH(dt1, 6, "den", "xanh");
+		gc1.hienthi();
+		DuongTronHH gc2 = new DuongTronHH;
+		gc2.nhap();
+		gc2.hienthi();
+		System.out.println("Chu vi cua duong tron gc2: "+ gc2.ChuVi());
+		System.out.println("Dien tich cua duong tron gc2: "+ gc2.DienTich());
+		gc2.DoiDT(1, 2);
+		if(gc1.catNhau(gc2)) {
+			System.out.println("gc1 va gc2 cat nhau");
+		}
+		else {
+			System.out.println("gc1 va gc2 khong cat nhau");
+		}
+		if(gc1.equals(gc2)) {
+			System.out.println("gc1 bang gc2");
+		}
+		else {
+			System.out.println("gc1 khong bang gc2");
+		}
+}
